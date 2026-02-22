c914c38aec2af38943ad9bc1d18b8dd846bc48b4
meysdays <igbokwevictor122@gmail.com>
2026-02-23 00:21:54 +0100

    fix image display on production

--- src/data.ts
+++ src/data.ts
@@ -1,69 +1,76 @@
+import jollof from "./assets/images/jollof.png";
+import swallow from "./assets/images/swallow.png";
+import grills from "./assets/images/grills.png";
+import sweets from "./assets/images/sweet.png";
+import jrice from "./assets/images/jrice.png";
+import egusi from "./assets/images/egusi.png";
+import tilapia from "./assets/images/Tilapia.png";
 export const Category = [
   {
     name: "Jollof Delights",
-    img: "../src/assets/images/jollof.png",
+    img: jollof,
   },
   {
     name: "Swallow & Soups",
-    img: "../src/assets/images/swallow.png",
+    img: swallow,
   },
   {
     name: "Grills & BBQ",
-    img: "../src/assets/images/grills.png",
+    img: grills,
   },
   {
     name: "Swwets Treats",
-    img: "../src/assets/images/sweet.png",
+    img: sweets,
   },
   {
     name: "Jollof Delights",
-    img: "../src/assets/images/swallow.png",
+    img: jollof,
   },
   {
     name: "Jollof Delights",
-    img: "../src/assets/images/grills.png",
+    img: jollof,
   },
 ];
 
 export const Chef = [
   {
     name: "Spicy Tilapia Pepper Soup",
-    img: "../src/assets/images/Tilapia.png",
+    img: tilapia,
     description:
       "A comforting and spicy soup with tender tilapia fish, a true Nigerian delicacy.",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Fried Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jollof,
     description:
       "Our signature Jollof rice, cooked to perfection, served with succulent fried chicken.",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Fried Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jollof,
     description:
       "Our signature Jollof rice, cooked to perfection, served with succulent fried chicken.",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Smoked Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jrice,
     description:
       "Our signature Jollof rice, cooked to perfection, served with succulent smoked chicken.",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Fried Chicken",
-    img: "../src/assets/images/jrice.png",
+    img: jrice,
     description:
       "Our signature Jollof rice, cooked to perfection, served with succulent fried chicken.",
     price: "₦3,500",
   },
   {
     name: "Egusi Soup & Pounded Yam",
-    img: "../src/assets/images/egusi.png",
+    img: egusi,
     description:
       "Rich and savory Egusi soup with assorted meats, paired with freshly pounded yam.",
     price: "₦3,500",
@@ -73,25 +80,25 @@ export const Chef = [
 export const Cart = [
   {
     name: "Spicy Tilapia Pepper Soup",
-    img: "../src/assets/images/Tilapia.png",
+    img: tilapia,
     description: "With plantain, extra pepper sauce",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Fried Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jollof,
     description: "With plantain, extra pepper sauce",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Fried Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jollof,
     description: "With plantain, extra pepper sauce",
     price: "₦3,500",
   },
   {
     name: "Jollof Rice & Smoked Chicken",
-    img: "../src/assets/images/jollof.png",
+    img: jrice,
     description: "With plantain, extra pepper sauce",
     price: "₦3,500",
   },
